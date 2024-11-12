152e127479f47c5e944b689dbe1e84a885dbd108
marcgar2 <marcgar2@student.42madrid.com>
2024-11-12 21:21:47 +0100

    Starting GNL (gl)

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcgar2 <marcgar2@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 19:24:12 by marcgar2          #+#    #+#             */
+/*   Updated: 2024/11/12 19:37:28 by marcgar2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE 42
+# define BUFFER_SIZE
+#endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
